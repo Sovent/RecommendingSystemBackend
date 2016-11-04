4aa3565a6dbef1a4fd3e0410c2a5d7db616522bc
Sovent <vmargelov@gmail.com>
2016-11-04 22:41:10 +0300

    added socialization controller for routing example

---
+++ RecommendingSystemBackend/RecommendingSystemBackend/Controllers/SocializationController.cs
@@ -0,0 +1,19 @@
+﻿using RecommendingSystemBackend.Models;
+using System.Web.Http;
+
+namespace RecommendingSystemBackend.Controllers
+{
+    public class SocializationController : ApiController
+    {
+        [HttpGet]
+        [Route("user/{userId}/matches")]
+        public MatchedUser[] MatchUsersFor(int userId)
+        {
+            return new[]
+            {
+                new MatchedUser(userId, 3),
+                new MatchedUser(userId, 13)
+            };
+        }
+    }
+}
---
+++ RecommendingSystemBackend/RecommendingSystemBackend/Models/MatchedUser.cs
@@ -0,0 +1,15 @@
+﻿namespace RecommendingSystemBackend.Models
+{
+    public class MatchedUser
+    {
+        public MatchedUser(int targetUserId, int matchedUserId)
+        {
+            TargetUserId = targetUserId;
+            MatchedUserId = matchedUserId;
+        }
+
+        public int TargetUserId { get; private set; }
+
+        public int MatchedUserId { get; private set; }
+    }
+}
